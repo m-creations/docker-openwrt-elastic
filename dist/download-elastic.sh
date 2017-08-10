9c9c64f3932d9c5473dca9ff3d7282d35e5ddb64
Ioanna M. Dimitriou <ioanna.m.dimitriou@gmail.com>
2017-08-10 14:59:35 +0200

    removed unused variable

--- dist/download-elastic.sh
+++ dist/download-elastic.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Download and place elastictomcat packs in this dist/ directory to avoid multiple times download.
-ELASTIC_MAJOR=2.3
 ELASTIC_VERSION=2.3.1
 ELASTIC_REPO_BASE=https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch
 ELASTIC_ARTIFACT_NAME=elasticsearch-${ELASTIC_VERSION}
